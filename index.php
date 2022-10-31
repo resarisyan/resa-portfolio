e228db19a788876e21e9c2bce03da94aa0d30e42
resarisyan <resarisyan77@gmail.com>
2022-11-01 03:22:07 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,415 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+		<link rel="stylesheet" href="./css/style.css">
+		<link rel="stylesheet" href="./css/responsive-style.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <!-- Favicon -->
+		<link rel="apple-touch-icon" sizes="57x57" href="./images/favicon/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="./images/favicon/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="./images/favicon/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="./images/favicon/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="./images/favicon/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="./images/favicon/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="./images/favicon/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="./images/favicon/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="./images/favicon/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="./images/favicon/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="./images/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="./images/favicon/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="./images/favicon/favicon-16x16.png">
+		<!-- <link rel="manifest" href="./images/favicon//manifest.json"> -->
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="./images/favicon//ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+		<title>Resa Auliana Risyan</title>
+	</head>
+	<body data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="75">
+		<!-- Navbar -->
+		<header class="header_wrapper">
+			<nav class="navbar navbar-expand-lg fixed-top">
+				<div class="container">
+					<a class="navbar-brand" href="#">
+					<img src="./images/logo.png">
+					</a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<!-- <span class="navbar-toggler-icon"></span> -->
+						<i class="fas fa-stream navbar-toggler-icon"></i>
+					</button>
+					<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+						<ul class="navbar-nav menu-navbar-nav">
+							<li class="nav-item">
+								<a class="nav-link" href="#home">Home</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#about">About</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#services">Services</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#portfolio">Portfolio</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#contact">Contact</a>
+							</li>
+							<li class="nav-item mt-3 mt-lg-0">
+								<a class="main-btn" href="#contact">Hire me</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</header>
+		<!-- Banner -->
+		<section id="home" class="banner_wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 text-center text-md-start">
+						<h6>WELCOME TO MY WORLD</h6>
+						<h1>I'm Resa <br>
+							<span>Full Stack Developer</span>
+							<br>Base In Indonesia
+						</h1>
+						<div class="mt-4">
+							<a href="https://shorturl.at/cijoA" class="main-btn" target="_blank">Download CV</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- About -->
+		<section id="about" class="about_wrapper">
+			<div class="container">
+				<div class="row justify-content-between align-items-center">
+					<div class="col-lg-5 mb-4 mb-lg-0">
+						<img src="./images/about.jpg" class="img-fluid rounded-3" data-aos="fade-up">
+					</div>
+					<div class="col-lg-7 ps-lg-5 text-center text-lg-start">
+						<div class="my-3 my-lg-0">
+							<span class="subtitle">My About Details</span>
+							<h2>About Me</h2>
+							<p>Sebagai seorang yang aktif dalam pemrograman. Saya sudah memahai konsep, menguasai beberapa teknologi serta mengerjakan persoalan pemrograman dengan baik. Saya mampu membuat aplikasi dengan jenis platform yang berbeda. Kemampuan pemrograman yang saya kuasai sekarang cukup untuk membuat suatu produk atau aplikasi yang dibutuhkan suatu perusahaan.</p>
+						</div>
+					</div>
+					<div class="pt-4">
+						<ul class="nav nav-pills justify-content-center justify-content-lg-between mb-3" id="pills-tab" role="tablist">
+							<li class="nav-item" role="presentation">
+								<button class="nav-link active" id="pills-skill-tab" data-bs-toggle="pill" data-bs-target="#pills-skill" type="button" role="tab" aria-controls="pills-skill" aria-selected="true">Main Skills</button>
+							</li>
+							<li class="nav-item" role="presentation">
+								<button class="nav-link" id="pills-award-tab" data-bs-toggle="pill" data-bs-target="#pills-award" type="button" role="tab" aria-controls="pills-award" aria-selected="false">Certificate</button>
+							</li>
+							<li class="nav-item" role="presentation">
+								<button class="nav-link" id="pills-experiance-tab" data-bs-toggle="pill" data-bs-target="#pills-experiance" type="button" role="tab" aria-controls="pills-experiance" aria-selected="false">Experiance</button>
+							</li>
+							<li class="nav-item" role="presentation">
+								<button class="nav-link" id="pills-education-tab" data-bs-toggle="pill" data-bs-target="#pills-education" type="button" role="tab" aria-controls="pills-education" aria-selected="false">Education</button>
+							</li>
+						</ul>
+						<div class="tab-content" id="pills-tabContent">
+							<div class="tab-pane fade show active" id="pills-skill" role="tabpanel" aria-labelledby="pills-skill-tab">
+								<div class="single-progress">
+									<h6>PHP</h6>
+									<div class="progress">
+										<div class="progress-bar" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+									</div>
+									<span class="label">90%</span>
+								</div>
+								<div class="tab-pane fade show active" id="pills-skill" role="tabpanel" aria-labelledby="pills-skill-tab">
+									<div class="single-progress">
+										<h6>JavaScript</h6>
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+										<span class="label">85%</span>
+									</div>
+								</div>
+								<div class="tab-pane fade show active" id="pills-skill" role="tabpanel" aria-labelledby="pills-skill-tab">
+									<div class="single-progress">
+										<h6>Java</h6>
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+										<span class="label">80%</span>
+									</div>
+								</div>
+								<div class="tab-pane fade show active" id="pills-skill" role="tabpanel" aria-labelledby="pills-skill-tab">
+									<div class="single-progress">
+										<h6>Dart</h6>
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+										<span class="label">70%</span>
+									</div>
+								</div>
+							</div>
+							<div class="tab-pane fade" id="pills-award" role="tabpanel" aria-labelledby="pills-award-tab">
+								<div class="row image-grid">
+									<figure class="col-lg-4 col-md-12 mb-4 mb-lg-0 item">
+										<a class="d-block" href="">
+										<img src="./images/certificate/c1.jpg" class="w-100 shadow-1-strong rounded mb-4">
+										</a>
+									</figure>
+									<figure class="col-lg-4 col-md-12 mb-4 mb-lg-0 item">
+										<a class="d-block" href="">
+										<img src="./images/certificate/c2.jpg" class="w-100 shadow-1-strong rounded mb-4">
+										</a>
+									</figure>
+									<figure class="col-lg-4 col-md-12 mb-4 mb-lg-0 item">
+										<a class="d-block" href="">
+										<img src="./images/certificate/c3.jpg" class="w-100 shadow-1-strong rounded mb-4">
+										</a>
+									</figure>
+									<figure class="col-lg-4 col-md-12 mb-4 mb-lg-0 item">
+										<a class="d-block" href="">
+										<img src="./images/certificate/c4.jpg" class="w-100 shadow-1-strong rounded mb-4">
+										</a>
+									</figure>
+									<figure class="col-lg-4 col-md-12 mb-4 mb-lg-0 item">
+										<a class="d-block" href="">
+										<img src="./images/certificate/c5.jpg" class="w-100 shadow-1-strong rounded mb-4">
+										</a>
+									</figure>
+									<figure class="col-lg-4 col-md-12 mb-4 mb-lg-0 item">
+										<a class="d-block" href="">
+										<img src="./images/certificate/c6.jpg" class="w-100 shadow-1-strong rounded mb-4">
+										</a>
+									</figure>
+								</div>
+							</div>
+							<div class="tab-pane fade" id="pills-experiance" role="tabpanel" aria-labelledby="pills-experiance-tab">
+								<ul class="text-start ps-0">
+									<li>
+										<a href="javascript:void(0)"> Internship Technician <span> - PT. Telkom Access Cianjur</span>
+										</a> MAR 2019 - MAY 2019
+									</li>
+									<li>
+										<a href="javascript:void(0)"> Freelancer Web Programmer <span> - Work From Home</span>
+										</a> APR 2020 - Current
+									</li>
+									<li>
+										<a href="javascript:void(0)"> Laboratory Assistant <span> - Suryakancana University</span>
+										</a> AGU 2021 - Current
+									</li>
+									<li>
+										<a href="javascript:void(0)"> Internship Backend Programmer <span> - PT. ASQI Digital Innovation</span>
+										</a> OCT 2022 - Current
+									</li>
+								</ul>
+							</div>
+							<div class="tab-pane fade" id="pills-education" role="tabpanel" aria-labelledby="pills-education-tab">
+								<ul class="text-start ps-0">
+									<li>
+										<a href="javascript:void(0)"> Primary School Ibu Jenab 1 <span> - Cianjur</span>
+										</a> 2008 - 2014
+									</li>
+									<li>
+										<a href="javascript:void(0)"> Junior High School 1 <span> - Cianjur</span>
+										</a> 2014 - 2017
+									</li>
+									<li>
+										<a href="javascript:void(0)"> Vocational High School 1 <span> - Cianjur</span>
+										</a> 2017 - 2020
+									</li>
+									<li>
+										<a href="javascript:void(0)"> Suryakancana University <span> - Cianjur</span>
+										</a> 2020 - Current
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Services -->
+		<section id="services" class="services_wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 text-center mb-4">
+						<span class="subtitle">What I can do for you</span>
+						<h2>My Awesome Services</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit, <br class="d-none d-md-block"> Lorem ipsum dolor sit amet consectetur. </p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-4 col-md-6 mb-4">
+						<div class="card" data-aos="fade-up">
+							<div class="card-body text-md-start text-center">
+								<img src="./images/service/service-2.svg" class="img-fluid mb-4">
+								<h3>Website App Development</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A voluptatum repellat non vero nostrum animi dignissimos fugiat inventore modi adipisci!</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 mb-4">
+						<div class="card" data-aos="fade-up">
+							<div class="card-body text-md-start text-center">
+								<img src="./images/service/service-3.svg" class="img-fluid mb-4">
+								<h3>Desktop App Development</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A voluptatum repellat non vero nostrum animi dignissimos fugiat inventore modi adipisci!</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 mb-4">
+						<div class="card" data-aos="fade-up">
+							<div class="card-body text-md-start text-center">
+								<img src="./images/service/service-1.svg" class="img-fluid mb-4">
+								<h3>Mobile App Development</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A voluptatum repellat non vero nostrum animi dignissimos fugiat inventore modi adipisci!</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</section>
+		<!-- Portfolio -->
+		<section id="portfolio" class="portfolio_wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 text-center mb-4">
+						<span class="subtitle">My Complete Project</span>
+						<h2>My Latest Project</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit, <br class="d-none d-md-block"> Lorem ipsum dolor sit amet consectetur. </p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-4 col-md-6 mb-4">
+						<div class="card p-0" data-aos="fade-down">
+							<span style="background-image: url(./images/app/app1.png);"></span>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 mb-4">
+						<div class="card p-0" data-aos="fade-up">
+							<span style="background-image: url(./images/app/app2.png);"></span>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 mb-4">
+						<div class="card p-0" data-aos="fade-up">
+							<span style="background-image: url(./images/app/app3.png);"></span>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 mb-4">
+						<div class="card p-0" data-aos="fade-up">
+							<span style="background-image: url(./images/app/app4.png);"></span>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 mb-4">
+						<div class="card p-0" data-aos="fade-up">
+							<span style="background-image: url(./images/app/app5.png);"></span>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 mb-4">
+						<div class="card p-0" data-aos="fade-up">
+							<span style="background-image: url(./images/app/app6.png);"></span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Contact Section -->
+		<section id="contact" class="contact_wrapper">
+			<div class="container">
+				<div class="row justify-content-between align-items-center">
+					<div class="col-lg-6 order-1 mb-4 order-lg-1 mb-lg-0">
+						<img src="./images/contact.jpg" class="img-fluid">
+					</div>
+					<div class="col-lg-6 order-2 order-lg-1 pe-lg-5 text-lg-start text-center">
+						<span class="subtitle">Contact Me</span>
+						<h2>Hire Me.</h2>
+						<div class="row call_details mb-4">
+							<label class="col-sm3 col-lg-4">Call Me Directly:</label>
+							<div class="col-sm-9 col-lg-8 mb-3 mb-lg-2 text-md-start">
+								<a href="javascript:void(0)">+6287749355656</a>
+							</div>
+							<label class="col-sm3 col-lg-4">Contact Email:</label>
+							<div class="col-sm-9 col-lg-8 mb-3 mb-lg-2 text-md-start">
+								<a href="mailto:resarisyan77@gmail.com">resarisyan77@gmail.com</a>
+							</div>
+						</div>
+						<form data-aos="fade-up">
+							<div class="mb-4">
+								<input type="text" class="form-control" placeholder="Your Name..." autocomplete="off">
+							</div>
+							<div class="mb-4">
+								<input type="number" class="form-control" placeholder="Your Phone..." autocomplete="off">
+							</div>
+							<div class="mb-4">
+								<input type="email" class="form-control" placeholder="Your Email..." autocomplete="off">
+							</div>
+							<div class="mb-4">
+								<textarea cols="40" rows="4" class="form-control" id="message" placeholder="Write a Message..." autocomplete="off"></textarea>
+							</div>
+							<button type="submit" class="main-btn">Submit</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Footer -->
+		<section id="contact" class="footer_wrapper mt-3 mt-md-0">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-4 col-md-6 text-center text-md-start">
+						<div class="footer-logo mb-3 mb-md-0">
+							<img src="./images/logo.png" class="img-fluid">
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6">
+						<ul class="list-unstyled d-flex justify-content-center justify-md-end justify-content-lg-center social-icon mb-3 mb-md-0">
+							<li>
+								<a href="https://www.instagram.com/resarisyan/" target="_blank">
+								<i class="fab fa-instagram"></i>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.facebook.com/rarisyan/" target="_blank">
+								<i class="fab fa-facebook-f"></i>
+								</a>
+							</li>
+							<li>
+								<a href="https://github.com/resarisyan/" target="_blank">
+								<i class="fa-brands fa-github"></i>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.linkedin.com/in/resa-risyan/" target="_blank">
+								<i class="fab fa-linkedin-in"></i>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-lg-4 col-md-12">
+						<div class="copyright-text text-lg-start text-center mb-3 mb-lg-0">
+							<p class="mb-0">Copyright © 2022 <a href="#">resarisyan</a>. All Rights Reserved. </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<div class="modal lightbox-modal" id="lightbox-modal" tabindex="-1">
+			<div class="modal-dialog modal-fullscreen my-5">
+				<div class="modal-content">
+					<button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+					<div class="modal-body">
+						<div class="container-fluid p-0">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+		<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="./js/main.js"></script>
+    <script>
+      AOS.init();
+    </script>
+	</body>
+</html>
